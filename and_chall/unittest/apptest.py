787e2b26b194968cb8c08e6292cd0f5885265f4b
Brenden <brendenodhiambo@gmail.com>
2017-07-20 15:46:54 +0300

    unit test template

--- and_chall/unittest/apptest.py
+++ and_chall/unittest/apptest.py
@@ -1,7 +1,31 @@
 import unittest
 
-class appTest(unittest.TestCase):
+class LearningCase(unittest.TestCase):
+    def register(self):
+        self.assertEqual(1, 1)
+	
+    def login(self):
+        self.assertEqual(1, 1)
+    
+
+     def createBucketAct(self):
+        self.assertEqual(1, 1)
+
+     def addActTask(self):
+        self.assertEqual(1, 1)
+
+     def delActTask(self):
+        self.assertEqual(1, 1)
+     
+
+      def delBucketAct(self):
+        self.assertEqual(1, 1)
 
 
-if __name__ == '__main__':
+
+
+def main():
     unittest.main()
+
+if __name__ == "__main__":
+    main()
